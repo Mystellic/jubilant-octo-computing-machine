4693a04309663cd6fe2319a26a7a790ff5ae6e45
Wobbe <55002389+Mystellic@users.noreply.github.com>
2019-12-04 15:45:56 +0100

    Updated Crack_stress to plot stress along span

--- Crack_stress.py
+++ Crack_stress.py
@@ -5,15 +5,34 @@ Calculate crack size or stress
 """
 
 import math as m
-
-
-Kc = 27.5e6
+import numpy as np
+import matplotlib.pyplot as plt
 
 def stress(a):
-    stress = Kc/m.sqrt(m.pi*a)
+    stress = Kc/np.sqrt(m.pi*a)
     return stress
 
 
 def cracksize(s):
     cracksize = (Kc*Kc)/(s*s)*m.pi
     return cracksize
+
+
+
+#-----------------constants-----------------#
+
+#Kc = 40e6      #Titanium
+Kc = 27.5e6  #Aluminium
+alu_failure_stress = 483e6
+a = 0.025
+
+
+#------------------Arrays------------------#
+
+span = np.linspace(0, 10.8695, 600)
+cracks = np.linspace(a, a, 600)
+crack_stress = stress(cracks)
+crack_safety = alu_failure_stress/crack_stress
+
+plt.plot(span,crack_safety)
+plt.show()
